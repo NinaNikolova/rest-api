c221b8f13e413e8c2db02542ab5359c39b30bfd9
nina <ninagbs@abv.bg>
2023-07-21 11:53:00 +0300

    changes in Item.js

--- middlewares/session.js
+++ middlewares/session.js
@@ -3,6 +3,7 @@ const { parseToken } = require('../services/userService');
 
 module.exports = () => (req, res, next) => {
     const token = req.headers['x-authorization'];
+    // const token = req.cookies.token;
 
     if (token) {
         try {
--- models/Item.js
+++ models/Item.js
@@ -2,12 +2,12 @@ const { Schema, model, Types: { ObjectId } } = require('mongoose');
 
 
 const itemSchema = new Schema({
-    title: { type: String, required: true, minlength: [3, 'Make must be at least 3 characters long'] },
-    ingredients: { type: String, required: true, minlength: [3, 'Model must be at least 3 characters long'] },
+    title: { type: String, required: true, minlength: [3, 'Title must be at least 3 characters long'] },
+    ingredients: { type: String, required: true, minlength: [3, 'Ingredients must be at least 3 characters long'] },
     time: {
         type: Number, required: true, validate: {
             validator: value => value >= 0 && value <= 300,
-            message: 'Year must be between 0 and 24'
+            message: 'Time must be between 0 and 300 minutes'
         }
     },
     description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
